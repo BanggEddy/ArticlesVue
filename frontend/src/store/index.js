1349dbba488c8973933af7974c32df5a68a96b7f
David LI <108392457+BanggEddy@users.noreply.gitlab.com>
2024-09-19 16:28:25 +0200

    store à faire

---
+++ frontend/src/store/index.js
@@ -0,0 +1,11 @@
+import { createStore } from "vuex";
+import jwtDecode from "jwt-decode";
+
+const store = createStore({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+});
+
+export default store;
